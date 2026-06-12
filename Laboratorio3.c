59b3b9e0f87d6dc0ca3fb9ede61cd7de7e4813de
Fermín <ferminndelgado@gmail.com>
2026-06-12 14:58:27 -0300

    Nacen los hijos y repartimos los walkie-talkies

--- Laboratorio3.c
+++ Laboratorio3.c
@@ -57,6 +57,53 @@ int main() {
 
     printf("Padre: Memoria, semáforo y pipes listos.\n");
 
+    // 4. Creamos el primer hijo (Proceso Crédito)
+    pid_credito = fork();
+
+    if (pid_credito < 0) {
+        perror("Error al crear el hijo credito");
+        exit(1);
+    } else if (pid_credito == 0) {
+        // --- ESTE BLOQUE SOLO LO EJECUTA EL HIJO 1 ---
+        // El hijo 1 solo usa su pipe para enviarle (escribir) al padre.
+        close(pipe_credito[0]); // Por seguridad, cerramos la "oreja" (lectura) de su walkie-talkie
+        close(pipe_debito[0]);  // Tampoco usa el pipe del otro hermano
+        close(pipe_debito[1]);
+
+        // Lo mandamos a trabajar con su función (la escribimos más adelante)
+        credito("credito.txt", pipe_credito, memoria);
+        
+        // Cuando termina su trabajo, el hijo DEBE morir acá. No sigue de largo.
+        exit(0); 
+    }
+
+    // 5. Creamos el segundo hijo (Proceso Débito)
+    // Ojo: Solo el PADRE sobrevive para llegar a esta línea
+    pid_debito = fork();
+
+    if (pid_debito < 0) {
+        perror("Error al crear el hijo debito");
+        exit(1);
+    } else if (pid_debito == 0) {
+        // --- ESTE BLOQUE SOLO LO EJECUTA EL HIJO 2 ---
+        close(pipe_debito[0]);  // Cerramos la "oreja" (lectura) de su walkie-talkie
+        close(pipe_credito[0]); // No usa el pipe del hermano
+        close(pipe_credito[1]);
+
+        // Lo mandamos a trabajar
+        debito("debito.txt", pipe_debito, memoria);
+        
+        exit(0); // El hijo 2 termina su vida acá.
+    }
+
+    // 6. --- ESTE BLOQUE SOLO LO EJECUTA EL PADRE ---
+    // Como el padre solo va a ESCUCHAR (leer) lo que le mandan los hijos,
+    // cerramos la "boca" (escritura) de ambos walkie-talkies.
+    close(pipe_credito[1]);
+    close(pipe_debito[1]);
+
+    printf("Padre: Hijos creados correctamente. Esperando los reportes...\n");
+
 
     return 0;
 }
